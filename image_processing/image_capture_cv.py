d84e8038c4e1c3bc06269905ebe9b3347cad655d
s_du <sungju29@g.skku.edu>
2022-10-28 08:28:51 +0900

    image_capture_cv.py folder name, video path 2개 수정해서 실행하면 됨

---
+++ image_processing/image_capture_cv.py
@@ -0,0 +1,47 @@
+import cv2
+import os
+from time import time, ctime
+
+def createFolder(directory):
+    try:
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+    except OSError:
+        print("Error: Creating directory." + directory)
+
+
+class image_capture:
+    def __init__(self, frame_divide):
+        self.img_cnt = 0
+        self.cnt = 0
+        self.folder_name = "" + str(ctime(time()))
+        self.video_path = ""
+        createFolder(self.folder_name)
+        self.capture(frame_divide)
+    
+    def capture(self, frame_devide):
+        vidcap = cv2.VideoCapture(self.video_path)
+        while (vidcap.isOpened()):
+            ret, img = vidcap.read()
+
+            if not ret:
+                print("No video")
+                break
+
+            if self.img_cnt % self.frame_divide == 0:
+                photo_name = self.folder_name + "/" + str(self.cnt) + ".jpg"
+                cv2.imwrite(photo_name, img)
+                print("Photo %d is saved." % (self.cnt))
+                self.cnt += 1
+
+            self.img_cnt += 1
+
+
+if __name__ == '__main__':
+    frame_divide = 5
+    ic = image_capture(frame_divide)
+    
+    print("-----Starting Image Capture-----")
+    print("Extract 1 picture from %d frames", frame_divide)
+    
+    cv2.destroyAllWindows()
